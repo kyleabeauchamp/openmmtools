f21a8f0b6d561e526a07ff10a3dbdff9ea94e69f
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-01-28 10:16:49 -0500

    Added preliminary ramping hmc

--- openmmtools/integrators.py
+++ openmmtools/integrators.py
@@ -737,3 +737,140 @@ class VVVRIntegrator(simtk.openmm.CustomIntegrator):
         self.addConstrainVelocities();
 
 
+class RampedHMCIntegrator(simtk.openmm.CustomIntegrator):
+    """
+    Hybrid Monte Carlo (HMC) integrator.
+
+    """
+
+    def __init__(self, temperature=298.0*simtk.unit.kelvin, nsteps=10, timestep=1*simtk.unit.femtoseconds, ramping=0.1):
+        """
+        Create a hybrid Monte Carlo (HMC) integrator.
+
+        Parameters
+        ----------
+        temperature : numpy.unit.Quantity compatible with kelvin, default: 298*simtk.unit.kelvin
+           The temperature.
+        nsteps : int, default: 10
+           The number of velocity Verlet steps to take per HMC trial.
+        timestep : numpy.unit.Quantity compatible with femtoseconds, default: 1*simtk.unit.femtoseconds
+           The integration timestep.
+
+        Warning
+        -------
+        Because 'nsteps' sets the number of steps taken, a call to integrator.step(1) actually takes 'nsteps' steps.
+
+        Notes
+        -----
+        The velocity is drawn from a Maxwell-Boltzmann distribution, then 'nsteps' steps are taken,
+        and the new configuration is either accepted or rejected.
+
+        Additional global variables 'ntrials' and  'naccept' keep track of how many trials have been attempted and
+        accepted, respectively.
+
+        TODO
+        ----
+        Currently, the simulation timestep is only advanced by 'timestep' each step, rather than timestep*nsteps.  Fix this.
+
+        Examples
+        --------
+
+        Create an HMC integrator.
+
+        >>> timestep = 1.0 * simtk.unit.femtoseconds # fictitious timestep
+        >>> temperature = 298.0 * simtk.unit.kelvin
+        >>> nsteps = 10 # number of steps per call
+        >>> integrator = HMCIntegrator(temperature, nsteps, timestep)
+
+        """
+
+        super(RampedHMCIntegrator, self).__init__(timestep)
+
+        # Compute the thermal energy.
+        kT = kB * temperature
+
+        #
+        # Integrator initialization.
+        #
+        self.addGlobalVariable("naccept", 0) # number accepted
+        self.addGlobalVariable("ntrials", 0) # number of Metropolization trials
+
+        self.addGlobalVariable("kT", kT) # thermal energy
+        self.addPerDofVariable("sigma", 0)
+        self.addGlobalVariable("ke", 0) # kinetic energy
+        self.addPerDofVariable("xold", 0) # old positions
+        self.addGlobalVariable("Eold", 0) # old energy
+        self.addGlobalVariable("Enew", 0) # new energy
+        self.addGlobalVariable("accept", 0) # accept or reject
+        self.addPerDofVariable("x1", 0) # for constraints
+
+        #
+        # Pre-computation.
+        # This only needs to be done once, but it needs to be done for each degree of freedom.
+        # Could move this to initialization?
+        #
+        self.addComputePerDof("sigma", "sqrt(kT/m)")
+
+        #
+        # Allow Context updating here, outside of inner loop only.
+        #
+        self.addUpdateContextState();
+
+        #
+        # Draw new velocity.
+        #
+        self.addComputePerDof("v", "sigma*gaussian")
+        self.addConstrainVelocities();
+
+        #
+        # Store old position and energy.
+        #
+        self.addComputeSum("ke", "0.5*m*v*v")
+        self.addComputeGlobal("Eold", "ke + energy")
+        self.addComputePerDof("xold", "x")
+
+        #
+        # Inner symplectic steps using velocity Verlet.
+        #
+        np = numpy
+        f = lambda alpha, n: np.exp(np.linspace(0, 1, n) * alpha)
+        g = lambda alpha, n: n * f(alpha, n) / f(alpha, n).sum()
+        alpha_grid = g(ramping, nsteps)
+        
+        for step in range(nsteps):
+            
+            self.addComputePerDof("v", "v+%f*0.5*dt*f/m" % alpha_grid[step])
+            self.addComputePerDof("x", "x+%f*dt*v" % alpha_grid[step])
+            self.addComputePerDof("x1", "x")
+            self.addConstrainPositions()
+            self.addComputePerDof("v", "v+%f*0.5*dt*f/m+(x-x1)/dt" % alpha_grid[step])
+            self.addConstrainVelocities()
+
+        #
+        # Accept/reject step.
+        #
+        self.addComputeSum("ke", "0.5*m*v*v")
+        self.addComputeGlobal("Enew", "ke + energy")
+        self.addComputeGlobal("accept", "step(exp(-(Enew-Eold)/kT) - uniform)")
+        self.addComputePerDof("x", "x*accept + xold*(1-accept)")
+
+        #
+        # Accumulate statistics.
+        #
+        self.addComputeGlobal("naccept", "naccept + accept")
+        self.addComputeGlobal("ntrials", "ntrials + 1")
+
+    @property
+    def n_accept(self):
+        """The number of accepted HMC moves."""
+        return self.getGlobalVariableByName("naccept")
+
+    @property
+    def n_trials(self):
+        """The total number of attempted HMC moves."""
+        return self.getGlobalVariableByName("ntrials")
+
+    @property
+    def acceptance_rate(self):
+        """The acceptance rate: n_accept  / n_trials."""
+        return self.n_accept / float(self.n_trials)
